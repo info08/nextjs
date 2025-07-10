57de8420b6c62a43f8e932bc68df6d9e63466b4a
Prashant Mishra <info.prashant08@gmail.com>
2025-07-11 03:24:21 +0530

    js

--- src/app/profile/[id]/page.js
+++ src/app/profile/[id]/page.js
@@ -1,20 +1,16 @@
 "use client";
-import React ,{FC} from 'react';  
 import Link from 'next/link';
 //import { useRouter } from 'next/navigation';
 //import { useState } from 'react';
 //import axios from 'axios';
 
-interface PageProps {
-  params: { id: string };
-}
 
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }) {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-96">
-        <h2 className="text-2xl font-bold mb-6 text-center">User Profile {params.id}</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">User Profile {params?.id}</h2>
         <p className="text-center">This is the user profile page.</p>
         <Link href="/profile" className="text-blue-600 hover:underline mt-4 block text-center">
           Go to profile
